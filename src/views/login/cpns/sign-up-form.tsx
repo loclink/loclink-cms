0532debbf7e3bbcdf08a9b842ced00183b49c864
loclink <loclink@163.com>
2022-08-01 15:27:01 +0800

    🌈 style: 注册表单结构样式修改

--- src/views/login/cpns/sign-up-form.tsx
+++ src/views/login/cpns/sign-up-form.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { LockOutlined, UserOutlined, KeyOutlined } from '@ant-design/icons';
 import { Button, Form, Input } from 'antd';
 
 const SignUpForm = memo(() => {
@@ -9,14 +9,20 @@ const SignUpForm = memo(() => {
 
   return (
     <div>
-      <Form name="normal_login" className="login-form" initialValues={{ remember: true }} onFinish={onFinish}>
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        labelCol={{ span: 5 }}
+      >
         <Form.Item
           label="帐号"
           colon
           name="username"
           rules={[{ required: true, message: 'Please input your Username!' }]}
         >
-          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          <Input prefix={<UserOutlined />} placeholder="账号" />
         </Form.Item>
         <Form.Item
           label="密码"
@@ -24,11 +30,20 @@ const SignUpForm = memo(() => {
           name="password"
           rules={[{ required: true, message: 'Please input your Password!' }]}
         >
-          <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password" />
+          <Input prefix={<LockOutlined />} type="password" placeholder="密码" />
+        </Form.Item>
+
+        <Form.Item
+          label="确认密码"
+          colon
+          name="confirmPassword"
+          rules={[{ required: true, message: 'Please input your Password!' }]}
+        >
+          <Input prefix={<LockOutlined />} type="password" placeholder="再次输入密码" />
         </Form.Item>
 
         <Form.Item label="注册码" colon name="signOnCode" rules={[{ required: true, message: '请输入注册码!' }]}>
-          <Input></Input>
+          <Input prefix={<KeyOutlined />} placeholder="注册码"></Input>
         </Form.Item>
 
         <Form.Item>
